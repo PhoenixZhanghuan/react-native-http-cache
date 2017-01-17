abdc5e11998344bee3b54f06e0ba7b351408aede
张欢 <zhanghuan@zhanghuandeMacBook-Pro.local>
2017-01-17 10:13:39 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by tdzl2_000 on 2015-12-29.
+ */
+import React, { Component } from 'react';
+import { NativeModules } from 'react-native';
+module.exports = NativeModules.HttpCache;
+
